cd6a333b5a765aabd2b04f8645ec89a379939eff
Antony Raphel <antonypr717@gmail.com>
2018-12-03 23:45:01 +0530

    Initial Commit

---
+++ ActivityTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ActivityTracker
+//
+//  Created by Antony Raphel on 03/12/18.
+//  Copyright © 2018 Antony Raphel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
